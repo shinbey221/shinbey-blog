0e001e77ac76ed19e07aea75f38ca0497d428cbe
shinji <shinbey221@gmail.com>
2020-02-15 17:03:01 +0900

    delete: 練習用に作ったcountのstoreを削除

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -1,11 +1,9 @@
 import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import Items from '~/store/items'
-import Count from '~/store/count'
 
 // eslint-disable-next-line import/no-mutable-exports
 let items: Items
-let count: Count
 
 /**
  * ストアを初期化する（型推論できるモジュールとして取得する）
@@ -14,7 +12,6 @@ let count: Count
 
 const initializeStores = (store: Store<any>): void => {
   items = getModule(Items, store)
-  count = getModule(Count, store)
 }
 
-export { initializeStores, items, count }
+export { initializeStores, items }
